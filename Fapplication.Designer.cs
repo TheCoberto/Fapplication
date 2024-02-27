f9ca82fb74d02e7b839aa5ce54628fa26936cdbf
TheCoberto <coby.kelly.ck@gmail.com>
2024-02-26 22:35:04 -0500

    Add project files.

---
+++ Fapplication.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Fapplication
+{
+    partial class Fapplication
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.ElapsedTime = new System.Windows.Forms.TextBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(12, 56);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(12, 101);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(12, 147);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(75, 23);
+            this.Reset.TabIndex = 2;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // ElapsedTime
+            // 
+            this.ElapsedTime.Location = new System.Drawing.Point(81, 12);
+            this.ElapsedTime.Name = "ElapsedTime";
+            this.ElapsedTime.Size = new System.Drawing.Size(100, 20);
+            this.ElapsedTime.TabIndex = 3;
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Fapplication
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 182);
+            this.Controls.Add(this.ElapsedTime);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Name = "Fapplication";
+            this.Text = "Fapplication";
+            this.FormClosing += Fapplication_FormClosing;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.TextBox ElapsedTime;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Fapplication.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Fapplication
+{
+    public partial class Fapplication : Form
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        private bool isRunning;
+        private string elapsedTimeFilePath = "elapsedTime.txt";
+
+        public Fapplication()
+        {
+            InitializeComponent();
+            string timeString = File.ReadAllText(elapsedTimeFilePath);
+            TimeSpan time = TimeSpan.Parse(timeString);
+            ElapsedTime.Text = time.ToString();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            if (!isRunning)
+            {
+                stopwatch.Start();
+                Timer.Start();
+                isRunning = true;
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                stopwatch.Stop();
+                Timer.Stop();
+                isRunning = false;
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            stopwatch.Reset();
+            ElapsedTime.Text = "00:00:00";
+            SaveElapsedTime();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            string timeString = File.ReadAllText(elapsedTimeFilePath);
+            TimeSpan time = TimeSpan.Parse(timeString);
+            time += stopwatch.Elapsed;
+            ElapsedTime.Text = time.ToString(@"hh\:mm\:ss");
+        }
+
+        private void Fapplication_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveElapsedTime();
+        }
+
+        private void SaveElapsedTime()
+        {
+            string timeString = File.ReadAllText(elapsedTimeFilePath);
+            TimeSpan time = TimeSpan.Parse(timeString);
+            TimeSpan totalTime = time + stopwatch.Elapsed;
+
+            try
+            {
+                if (stopwatch.Elapsed != TimeSpan.Zero)
+                {
+                    File.WriteAllText(elapsedTimeFilePath, totalTime.ToString());
+                }
+                else
+                {
+                    File.WriteAllText(elapsedTimeFilePath, TimeSpan.Zero.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to save elapsed time: " + ex.Message);
+            }
+        }
+    }
+}
